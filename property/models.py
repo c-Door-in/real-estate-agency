a5b0d6852172b2428c507b1fa4824000d5a9baf5
c_door In <55236660+git-tutorial-cmd@users.noreply.github.com>
2022-02-03 23:33:03 +0300

    Add model field Flat.new_building

--- property/models.py
+++ property/models.py
@@ -9,7 +9,7 @@ class Flat(models.Model):
         'Когда создано объявление',
         default=timezone.now,
         db_index=True)
-
+    new_building = models.NullBooleanField('Является ли новостройкой')
     description = models.TextField('Текст объявления', blank=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
 
